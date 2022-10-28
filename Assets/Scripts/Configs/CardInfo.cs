d33160ff59a6a206a558c69635a878d454da1e93
Pavel <cravt5@mail.ru>
2022-10-28 15:56:43 +0300

    Update Card System

--- Assets/Scripts/Configs/CardInfo.cs
+++ Assets/Scripts/Configs/CardInfo.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewCard", menuName = "Create Card/ Empty card", order = 51)]
+[CreateAssetMenu(fileName = "NewCard", menuName = "Create Card/ New card", order = 51)]
 public class CardInfo : ScriptableObject
 {
     [Header("Инфа о карте")]
-    [SerializeField] private string _id;
+    //[SerializeField] private string _id;
     [SerializeField] private Sprite _Image;
     public bool canBeSpawn;
 
@@ -15,17 +15,60 @@ public class CardInfo : ScriptableObject
     [SerializeField] private string _leftText;
     [SerializeField] private string _rightText;
 
+    [System.Serializable]
+    public class Parametrs
+    {
+        [Tooltip("Ключи: imba, goblin, key, money")]
+        public string key;
+        public float value;
+    }
+
+    [Header("При свайпе влево")]
+    public Parametrs[] _leftParametrsToChange;
+    public CardInfo[] _newCardOnLeft;
+    public bool _changeSpawnL;
+
+    [Header("При свайпе вправо")]
+    public Parametrs[] _rightParametrsToChange;
+    public CardInfo[] _newCardOnRight;
+    public bool _changeSpawnR;
+
     public virtual void LeftChoose()
     {
         Debug.Log("left Parametrs change");
+        foreach (var i in _leftParametrsToChange)
+        {
+            ControllerUI.inst.coeffManager.ChangeValue(i.key, i.value);
+        }
+        foreach (var i in _newCardOnLeft)
+        {
+            i.canBeSpawn = true;
+        }
+        if (_changeSpawnL)
+        {
+            this.canBeSpawn = false;
+        }
+
     }
 
     public virtual void RightChoose()
     {
         Debug.Log("Rigth Parametrs change");
+        foreach (var i in _rightParametrsToChange)
+        {
+            ControllerUI.inst.coeffManager.ChangeValue(i.key, i.value);
+        }
+        foreach (var i in _newCardOnRight)
+        {
+            i.canBeSpawn = true;
+        }
+        if (_changeSpawnR)
+        {
+            this.canBeSpawn = false;
+        }
     }
 
-    public string id => this._id;
+    //public string id => this._id;
     public Sprite Image => this._Image;
     
 
